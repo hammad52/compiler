66644013c010230d4ec115f025414fb5fbd35672
hammad52 <hammad52@rediffmail.com>
2018-07-06 15:50:11 +0530

    adding bean class for file details

---
+++ src/bean/FileDetails.java
@@ -0,0 +1,21 @@
+package bean;
+
+public class FileDetails {
+	String fileName,fileType;
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+}
--- src/compiler/CodeCompile.java
+++ src/compiler/CodeCompile.java
@@ -3,12 +3,16 @@ package compiler;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Date;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class CodeCompile
